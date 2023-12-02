1717c1fd16107df7a804d30606467e96b9967944
Rafik <rbikmaev@codemastersintl.com>
2023-12-02 19:36:55 +0300

    F

--- Lab2/Lab2/Main.Designer.cs
+++ Lab2/Lab2/Main.Designer.cs
@@ -24,81 +24,54 @@ namespace Lab2 {
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.btnPassTest = new System.Windows.Forms.Button();
-            this.btnInfo = new System.Windows.Forms.Button();
-            this.btnEditTest = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1.SuspendLayout();
-            this.SuspendLayout();
+            tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            btnPassTest = new System.Windows.Forms.Button();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Controls.Add(this.btnPassTest, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.btnInfo, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.btnEditTest, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(848, 57);
-            this.tableLayoutPanel1.TabIndex = 0;
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            tableLayoutPanel1.Controls.Add(btnPassTest, 1, 0);
+            tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new System.Drawing.Size(848, 57);
+            tableLayoutPanel1.TabIndex = 0;
             // 
             // btnPassTest
             // 
-            this.btnPassTest.Location = new System.Drawing.Point(285, 3);
-            this.btnPassTest.Name = "btnPassTest";
-            this.btnPassTest.Size = new System.Drawing.Size(276, 51);
-            this.btnPassTest.TabIndex = 1;
-            this.btnPassTest.Text = "Pass Test";
-            this.btnPassTest.UseVisualStyleBackColor = true;
-            this.btnPassTest.Click += new System.EventHandler(this.btnPassTest_Click);
-            // 
-            // btnInfo
-            // 
-            this.btnInfo.Location = new System.Drawing.Point(567, 3);
-            this.btnInfo.Name = "btnInfo";
-            this.btnInfo.Size = new System.Drawing.Size(278, 51);
-            this.btnInfo.TabIndex = 2;
-            this.btnInfo.Text = "Info";
-            this.btnInfo.UseVisualStyleBackColor = true;
-            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
-            // 
-            // btnEditTest
-            // 
-            this.btnEditTest.Location = new System.Drawing.Point(3, 3);
-            this.btnEditTest.Name = "btnEditTest";
-            this.btnEditTest.Size = new System.Drawing.Size(276, 51);
-            this.btnEditTest.TabIndex = 0;
-            this.btnEditTest.Text = "Edit Test";
-            this.btnEditTest.UseVisualStyleBackColor = true;
-            this.btnEditTest.Click += new System.EventHandler(this.btnEditTest_Click);
+            btnPassTest.Location = new System.Drawing.Point(285, 3);
+            btnPassTest.Name = "btnPassTest";
+            btnPassTest.Size = new System.Drawing.Size(276, 51);
+            btnPassTest.TabIndex = 1;
+            btnPassTest.Text = "Pass Test";
+            btnPassTest.UseVisualStyleBackColor = true;
+            btnPassTest.Click += btnPassTest_Click;
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(848, 575);
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "Main";
-            this.Text = "Main";
-            this.Load += new System.EventHandler(this.Main_Load);
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.Color.White;
+            ClientSize = new System.Drawing.Size(848, 575);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Main";
+            Text = "Main";
+            Load += Main_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Button btnEditTest;
         private System.Windows.Forms.Button btnPassTest;
-        private System.Windows.Forms.Button btnInfo;
     }
 }
 
